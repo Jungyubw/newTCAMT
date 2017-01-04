a8bdc7b3bfa06adcf35fab4251afa748788df7c3
Woo, Jungyub (Assoc) <jungyub.woo@nist.gov>
2017-01-04 13:51:18 -0500

    Domain class updated

--- tcamt-lite-domain/src/main/java/gov/nist/healthcare/tools/hl7/v2/tcamt/lite/domain/TestCaseGroup.java
+++ tcamt-lite-domain/src/main/java/gov/nist/healthcare/tools/hl7/v2/tcamt/lite/domain/TestCaseGroup.java
@@ -2,7 +2,6 @@ package gov.nist.healthcare.tools.hl7.v2.tcamt.lite.domain;
 
 import java.io.Serializable;
 import java.util.Comparator;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -16,38 +15,49 @@ public class TestCaseGroup extends TestCaseOrGroup implements Serializable, Clon
 	private static final long serialVersionUID = -8254402250986054606L;
 	
 	
-	private HashMap<String,String> testStoryContent=new HashMap<String, String>();
-
+	
 	
 	public TestCaseGroup() {
 		super();
 		this.type = "testcasegroup";
 	}
 
-	private Set<TestCase> testcases = new HashSet<TestCase>();
+	private Set<TestCaseOrGroup> children = new HashSet<TestCaseOrGroup>();
 
-	public Set<TestCase> getTestcases() {
-		return testcases;
-	}
-
-	public void setTestcases(Set<TestCase> testcases) {
-		this.testcases = testcases;
-	}
-
-	public void addTestCase(TestCase testcase) {
-		this.testcases.add(testcase);
+	public void addTestCase(TestCaseGroup testCaseOrGroup) {
+		this.children.add(testCaseOrGroup);
 	}
 	
+	
+	
+	public Set<TestCaseOrGroup> getChildren() {
+		return children;
+	}
+
+
+
+	public void setChildren(Set<TestCaseOrGroup> children) {
+		this.children = children;
+	}
+
+
+
 	@Override
 	public TestCaseGroup clone() throws CloneNotSupportedException {
 		TestCaseGroup cloned = (TestCaseGroup)super.clone();
 		cloned.setId(ObjectId.get().toString());
 		
-		Set<TestCase> cTestcases = new HashSet<TestCase>();
-		for(TestCase testcase:this.testcases){
-			cTestcases.add(testcase.clone());
+		Set<TestCaseOrGroup> cTestcaseOrGroup = new HashSet<TestCaseOrGroup>();
+		for(TestCaseOrGroup tcg:this.children){
+			if(tcg instanceof TestCase)  {
+				TestCase tc = (TestCase)tcg;
+				cTestcaseOrGroup.add(tc.clone());
+			}else{
+				TestCaseGroup tg = (TestCaseGroup)tcg;
+				cTestcaseOrGroup.add(tg.clone());
+			}
 		}
-		cloned.setTestcases(cTestcases);
+		cloned.setChildren(cTestcaseOrGroup);
 		
 		return cloned;
 	}
@@ -64,14 +74,6 @@ public class TestCaseGroup extends TestCaseOrGroup implements Serializable, Clon
 	public static void setTestCaseGroupPositionComparator(
 			Comparator<TestCaseGroup> testCaseGroupPositionComparator) {
 		TestCaseGroup.testCaseGroupPositionComparator = testCaseGroupPositionComparator;
-	}
-
-	public HashMap<String,String> getTestStoryContent() {
-		return testStoryContent;
-	}
-
-	public void setTestStoryContent(HashMap<String,String> testStoryContent) {
-		this.testStoryContent = testStoryContent;
 	}
 
 	public static Comparator<TestCaseGroup> testCaseGroupPositionComparator = new Comparator<TestCaseGroup>() {
