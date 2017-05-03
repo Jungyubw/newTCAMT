42e42d2628152656c34d8b329a1dc598c26ec043
Woo, Jungyub (Assoc) <jungyub.woo@nist.gov>
2017-05-03 16:47:54 -0400

    working

--- tcamt-lite-controller/src/main/java/gov/nist/healthcare/tools/hl7/v2/tcamt/lite/web/config/Bootstrap.java
+++ tcamt-lite-controller/src/main/java/gov/nist/healthcare/tools/hl7/v2/tcamt/lite/web/config/Bootstrap.java
@@ -1,12 +1,13 @@
 /**
- * This software was developed at the National Institute of Standards and Technology by employees
- * of the Federal Government in the course of their official duties. Pursuant to title 17 Section 105 of the
- * United States Code this software is not subject to copyright protection and is in the public domain.
- * This is an experimental system. NIST assumes no responsibility whatsoever for its use by other parties,
- * and makes no guarantees, expressed or implied, about its quality, reliability, or any other characteristic.
- * We would appreciate acknowledgement if the software is used. This software can be redistributed and/or
- * modified freely provided that any derivative works bear some notice that they are derived from it, and any
- * modified versions bear some notice that they have been modified.
+ * This software was developed at the National Institute of Standards and Technology by employees of
+ * the Federal Government in the course of their official duties. Pursuant to title 17 Section 105
+ * of the United States Code this software is not subject to copyright protection and is in the
+ * public domain. This is an experimental system. NIST assumes no responsibility whatsoever for its
+ * use by other parties, and makes no guarantees, expressed or implied, about its quality,
+ * reliability, or any other characteristic. We would appreciate acknowledgement if the software is
+ * used. This software can be redistributed and/or modified freely provided that any derivative
+ * works bear some notice that they are derived from it, and any modified versions bear some notice
+ * that they have been modified.
  */
 
 package gov.nist.healthcare.tools.hl7.v2.tcamt.lite.web.config;
@@ -40,138 +41,121 @@ import gov.nist.healthcare.tools.hl7.v2.tcamt.lite.service.TestStoryConfiguratio
 @Service
 public class Bootstrap implements InitializingBean {
 
-	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+  private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
-	@Autowired
-	TestPlanService testplanService;
+  @Autowired
+  TestPlanService testplanService;
 
-	@Autowired
-	ProfileService profileService;
+  @Autowired
+  ProfileService profileService;
 
-	@Autowired
-	TestStoryConfigurationService testStoryConfigurationService;
+  @Autowired
+  TestStoryConfigurationService testStoryConfigurationService;
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * org.springframework.beans.factory.InitializingBean#afterPropertiesSet()
-	 */
-	@Override
-	public void afterPropertiesSet() throws Exception {
-//		updateTCAMTProfiles();
-//		updateDefaultConfig();
-//		
-//		
-//		updateHL7Version();
-//		
-//		updateLongIDforTestPlan();
-	}
+  /*
+   * (non-Javadoc)
+   * 
+   * @see org.springframework.beans.factory.InitializingBean#afterPropertiesSet()
+   */
+  @Override
+  public void afterPropertiesSet() throws Exception {
+    updateHL7Version();
+    updateLongIDforTestPlan();
 
-	private void updateLongIDforTestPlan() throws TestPlanException{
-		List<TestPlan> tps = testplanService.findAll();
-		
-		for(TestPlan tp : tps){
-			if(tp.getLongId() == null){
-				long range = Long.MAX_VALUE;
-				Random r = new Random();
-				tp.setLongId((long)(r.nextDouble()*range));
-			}	
-			
-			for(TestCaseOrGroup tcog : tp.getChildren()){
-				visit(tcog);
-			}
-			
-			testplanService.save(tp);
-		}
-		
-	}
-	private void visit(TestCaseOrGroup tcog) {
-		if(tcog.getLongId() == null){
-			long range = Long.MAX_VALUE;
-			Random r = new Random();
-			tcog.setLongId((long)(r.nextDouble()*range));
-		}
-		
-		if(tcog instanceof TestCase){
-			TestCase tc = (TestCase)tcog;
-			for(TestStep child : tc.getTeststeps()){
-				if(child.getLongId() == null){
-					long range = Long.MAX_VALUE;
-					Random r = new Random();
-					child.setLongId((long)(r.nextDouble()*range));
-				}
-			}
-			
-		}else if(tcog instanceof TestCaseGroup){
-			TestCaseGroup group = (TestCaseGroup)tcog;
-			for(TestCaseOrGroup child : group.getChildren()){
-				visit(child);
-			}
-		}
-		
-	}
+    resetIGAMTProfile();
 
-	private void updateHL7Version() throws ProfileException {
-		List<Profile> profiles = profileService.findAll();
 
-		for (Profile p : profiles) {
-			if (p.getSourceType().equals("private")) {
-				String version = p.getMetaData().getHl7Version();
-				
-				for(Segment s: p.getSegments().getChildren()){
-					if(s.getHl7Version() == null){
-						s.setHl7Version(version);
-					}
-				}
-				
-				for(Datatype d: p.getDatatypes().getChildren()){
-					if(d.getHl7Version() == null){
-						d.setHl7Version(version);
-					}
-				}
-				
-				for(Table t: p.getTables().getChildren()){
-					if(t.getHl7Version() == null){
-						t.setHl7Version(version);
-					}
-				}
-		
-				profileService.save(p);
+  }
 
-			}
-		}
-		
-	}
+  private void resetIGAMTProfile() {
+    List<Profile> profiles = profileService.findAll();
+    for (Profile p : profiles) {
+      if (p.getSourceType().equals("igamt")) {
+        profileService.delete(p.getId());
+      }
+    }
 
-	public Logger getLogger() {
-		return logger;
-	}
 
-	private void updateTCAMTProfiles() throws ProfileException {
-		List<Profile> profiles = profileService.findAll();
+  }
 
-		for (Profile p : profiles) {
-			if (p.getSourceType() == null || p.getSourceType().isEmpty()) {
-				if (p.getAccountId().equals((long) 0)) {
-					p.setSourceType("public");
-				} else {
-					p.setSourceType("private");
-				}
-				profileService.save(p);
+  private void updateLongIDforTestPlan() throws TestPlanException {
+    List<TestPlan> tps = testplanService.findAll();
 
-			}
-		}
-	}
+    for (TestPlan tp : tps) {
+      if (tp.getLongId() == null) {
+        long range = Long.MAX_VALUE;
+        Random r = new Random();
+        tp.setLongId((long) (r.nextDouble() * range));
+      }
 
-	private void updateDefaultConfig() {
-		TestStoryConfiguration testStoryConfiguration = testStoryConfigurationService.findByAccountId((long) 0).get(0);
+      for (TestCaseOrGroup tcog : tp.getChildren()) {
+        visit(tcog);
+      }
 
-		for (TestStroyEntry tse : testStoryConfiguration.getTestStoryConfig()) {
-			if (tse.getId().equals("Description") || tse.getId().equals("Test Objectives")) {
-				tse.setSummaryEntry(true);
-			}
-		}
-		testStoryConfigurationService.save(testStoryConfiguration);
-	}
+      testplanService.save(tp);
+    }
+
+  }
+
+  private void visit(TestCaseOrGroup tcog) {
+    if (tcog.getLongId() == null) {
+      long range = Long.MAX_VALUE;
+      Random r = new Random();
+      tcog.setLongId((long) (r.nextDouble() * range));
+    }
+
+    if (tcog instanceof TestCase) {
+      TestCase tc = (TestCase) tcog;
+      for (TestStep child : tc.getTeststeps()) {
+        if (child.getLongId() == null) {
+          long range = Long.MAX_VALUE;
+          Random r = new Random();
+          child.setLongId((long) (r.nextDouble() * range));
+        }
+      }
+
+    } else if (tcog instanceof TestCaseGroup) {
+      TestCaseGroup group = (TestCaseGroup) tcog;
+      for (TestCaseOrGroup child : group.getChildren()) {
+        visit(child);
+      }
+    }
+
+  }
+
+  private void updateHL7Version() throws ProfileException {
+    List<Profile> profiles = profileService.findAll();
+
+    for (Profile p : profiles) {
+      if (p.getSourceType().equals("private")) {
+        String version = p.getMetaData().getHl7Version();
+
+        for (Segment s : p.getSegments().getChildren()) {
+          if (s.getHl7Version() == null) {
+            s.setHl7Version(version);
+          }
+        }
+
+        for (Datatype d : p.getDatatypes().getChildren()) {
+          if (d.getHl7Version() == null) {
+            d.setHl7Version(version);
+          }
+        }
+
+        for (Table t : p.getTables().getChildren()) {
+          if (t.getHl7Version() == null) {
+            t.setHl7Version(version);
+          }
+        }
+
+        profileService.save(p);
+
+      }
+    }
+
+  }
+
+  public Logger getLogger() {
+    return logger;
+  }
 }
